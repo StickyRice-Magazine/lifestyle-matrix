1007d5f514bacfe396c148d1b1f1b33b3cbef33b
Linh <linh@Linhs-MacBook-Air.local>
2021-01-14 17:20:09 -0500

    part II of Becoming Recollection of Home

--- lifestyle-matrix-mapper.js
+++ lifestyle-matrix-mapper.js
@@ -1,5 +1,13 @@
 const lifeStyleArticles = [
   {
+    link: "Becoming-Recollections-of-Home-Part-II",
+    image:
+      "https://freight.cargo.site/t/original/i/74c8d0803bf4d628c9b58052bb4751d0c16b73c759338e4703c8afc4c92d04b8/Thumbnail.jpg",
+    type: "Spotlight On",
+    name: "Becoming: Recollections of Home (Part II)",
+    description: 'Becoming: Jiā [家] (Part II) further reflects on fluctuating notions of home. Through a series of conversations and observations taken in Beijing, Becoming questions how notions of home shift and adapt over time.',
+  },
+  {
     link: "Becoming-Recollections-of-Home-Part-I",
     image:
       "https://freight.cargo.site/t/original/i/a9c60afacf36a68a806f33c761df59e5a53ec3a8349919c9c87fb9d9065eb741/Thumbnail_Resized-min-min.jpg",
